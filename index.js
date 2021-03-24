9d90e60e353a310e0ddfc4fadb8efa3cc9abde7a
Jenna Fritz <jennafritz24@gmail.com>
2021-03-24 16:19:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+const newContent = document.createTextNode("is the champion");
+newHeader.appendChild(newContent);
+document.body.appendChild(newHeader);
+
+
+
+
